e7852e37f955122065d71bd35538c78d5b3e158e
tatsenko-m <tatsenko.m@yandex.ru>
2023-07-19 21:46:26 +0300

    перенести свои предыдущие наработки по бэкенду из проекта mesto

---
+++ app.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const { errors } = require('celebrate');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/error-handler');
+const router = require('./routes/index');
+const { validateSignup, validateSignin } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000, DB_URL = 'mongodb://0.0.0.0:27017/bitfilmsdb' } = process.env;
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
+app.use(requestLogger);
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+});
+
+app.post('/signin', validateSignin, login);
+
+app.post('/signup', validateSignup, createUser);
+
+app.use(auth);
+
+app.use(router);
+
+app.listen(PORT);
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(errorHandler);
---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Неверный или истекший токен');
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    res.status(401);
+    return next(new UnauthorizedError('Неверный или истекший токен'));
+  }
+
+  req.user = payload;
+  return next();
+};
+
+module.exports = auth;
