2cae619bdc9b93aa07dc4e49a744a2a0088a2bab
tatsenko-m <tatsenko.m@yandex.ru>
2023-07-20 18:10:27 +0300

    fix: удалить ненужные роуты и контроллеры cards

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const movieRoutes = require('./movies');
-const cardRoutes = require('./cards');
 const NotFoundError = require('../errors/NotFoundError');
 
 router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
-router.use('/cards', cardRoutes);
 router.use('*', (req, res, next) => {
   next(new NotFoundError('Не найдено'));
 });
