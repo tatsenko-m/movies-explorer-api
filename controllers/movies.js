8783c3d33684686dd6d8ed1291269d8e170b6fa4
tatsenko-m <tatsenko.m@yandex.ru>
2023-07-20 18:02:24 +0300

    feat: создать контроллеры movies

---
+++ controllers/movies.js
@@ -0,0 +1,49 @@
+const Movie = require('../models/movie');
+const NotFoundError = require('../errors/NotFoundError');
+const BadRequestError = require('../errors/BadRequestError');
+const ForbiddenError = require('../errors/ForbiddenError');
+
+const getMovies = (req, res, next) => {
+  Movie.find({})
+    .then((movies) => res.status(200).send(movies))
+    .catch(next);
+};
+
+const createMovie = (req, res, next) => {
+  Movie.create({ ...req.body, owner: req.user._id })
+    .then((movie) => res.status(201).send(movie))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const deleteMovieById = (req, res, next) => {
+  Movie.findById(req.params._id)
+    .then((movie) => {
+      if (!movie) {
+        return next(new NotFoundError('Карточка не найдена'));
+      }
+      if (String(req.user._id) !== String(movie.owner)) {
+        return next(new ForbiddenError('Доступ запрещен'));
+      }
+      return Movie.findByIdAndRemove(req.params._id)
+        .then((deletedMovie) => res.status(200).send(deletedMovie));
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Некорректный id'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports = {
+  getMovies,
+  createMovie,
+  deleteMovieById,
+};
