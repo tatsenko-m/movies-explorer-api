05880a0136bd258db2fdb6a7bbf0cd9309d00a70
tatsenko-m <tatsenko.m@yandex.ru>
2023-07-21 23:30:54 +0300

    fix: исправить ошибку eslint в regex

--- constants/regex.js
+++ constants/regex.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line no-useless-escape
-module.exports.regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]+\.[a-zA-Z0-9()]+([-a-zA-Z0-9!@:%_.~#?&\/=\[\]$'()*+,;=-]*)#?/;
+module.exports.regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]+\.[a-zA-Z0-9()]+([-a-zA-Z0-9!@:%_.~#?&/=[\]$'()*+,;=-]*)#?/;
