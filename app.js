e7a9fda3a95887e2ea9e8510b4815bd448674ff2
tatsenko-m <tatsenko.m@yandex.ru>
2023-07-22 13:49:51 +0300

    refactor: вынести лимитер в отдельный файл

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
+const limiter = require('./middlewares/limiter');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error-handler');
@@ -16,11 +16,6 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000, DB_URL = 'mongodb://0.0.0.0:27017/bitfilmsdb' } = process.env;
 
 const app = express();
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
 
 app.use(cors());
 app.use(helmet());
